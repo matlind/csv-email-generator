61939ff1057a300a58d8ee7261e5d439dae01338
Matlind <matlind.ruka@zavamed.com>
2021-02-17 12:21:03 +0000

    adding the project

---
+++ index.js
@@ -0,0 +1,49 @@
+const axios = require("axios");
+const cron = require("node-cron");
+const { Parser } = require("json2csv");
+const { sendMail } = require("./send-email");
+
+
+fs = require("fs");
+
+axios.get("https://api.punkapi.com/v2/beers", {}).then(function (response) {
+  console.log(response.data.length);
+  const myData = [];
+  for (i = 0; i < response.data.length; i++) {
+    name = response.data[i].name;
+    abv = response.data[i].abv;
+    ibu = response.data[i].ibu;
+    myData.push({ name, abv, ibu });
+  }
+
+  const delimiter = ";";
+  const fields = ["name", "abv", "ibu"];
+  const opts = {
+    fields,
+    delimiter,
+  };
+
+  try {
+    const parser = new Parser(opts);
+    const csv = parser.parse(myData);
+
+    const fileName = "Matlind.csv";
+    fs.writeFileSync(fileName, csv);
+
+    cron.schedule(
+      " * * * * * * ",
+      () => {
+        console.log("Price updater email sent to Zava DE/Cs ");
+        sendMail(fileName);
+      },
+      {
+        schedule: true,
+        timezone: "Europe/Berlin",
+      }
+    );
+
+    // task.start()
+  } catch (err) {
+    console.error(err);
+  }
+});
---
+++ send-email.js
@@ -0,0 +1,34 @@
+require('dotenv').config()
+const nodemailer = require("nodemailer");
+
+var transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL_ADRESS,
+    pass: process.env.EMAIL_PASSWORD,
+  },
+});
+
+function mailOptionsFactory(fileName) {
+  return {
+    from: process.env.EMAIL_ADRESS,
+    to: process.env.EMAIL_RECIPIENT,
+    subject: "test mail",
+    html: "<h1>This is a test mail.</h1>",
+    attachments: [
+      {
+        filename: "text4.txt",
+        content: fs.createReadStream(fileName),
+      },
+    ],
+  };
+}
+
+function sendMail(fileName) {
+  transporter.sendMail(mailOptionsFactory(fileName), function (err, info) {
+    if (err) console.log(err);
+    else console.log("Emal sent: " + info.response);
+  });
+}
+
+module.exports.sendMail = sendMail;
